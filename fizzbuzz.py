c098a73917bd5c8c1a2af78a296bb7e61eda00c8
sainthm <sainthm@naver.com>
2022-09-23 15:17:36 +0900

    Edit fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 24+1):
-    if i%5==0:
-        print("buzz")
+    if i%15==0:
+        print("fizzbuzz")
     elif i%3==0:
         print("fizz")
-    elif i%15==0:
-        print("fizzbuzz")
+    elif i%5==0:
+        print("buzz")
     else:
         print(i)
