62cd7e93f8c498e9b262089db5a5b67636c22cd1
sainthm <sainthm@naver.com>
2022-09-23 15:49:41 +0900

    feat
    Edit on fibo-mem branch

--- fibo.py
+++ fibo.py
@@ -1,4 +1,12 @@
+cache = {0: 0, 1: 1}
+x = input()
+
 def fibonacci_of(n):
-    if n in {0, 1}:
-        return n
-        return fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
+    if n in cache:  # Base case
+        return cache[n]
+    # Compute and cache the Fibonacci number
+    cache[n] = fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
+    return cache[n]
+
+for n in range(int(x)):
+    print(fibonacci_of(n))
